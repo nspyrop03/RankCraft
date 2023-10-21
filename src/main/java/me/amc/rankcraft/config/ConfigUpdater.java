ad0ac58648f1a136ac2512509810c7599735d2f1
Nikolas Spyropoulos <nick.mccrafter@gmail.com>
2023-10-21 12:34:36 +0300

    Finished ConfigUpdater(?)

--- src/main/java/me/amc/rankcraft/config/ConfigUpdater.java
+++ src/main/java/me/amc/rankcraft/config/ConfigUpdater.java
@@ -22,29 +22,25 @@ public class ConfigUpdater {
      public void update() throws IOException {
           Set<String> keys = outConfig.getKeys(true);
           HashMap<String, Object> map = new HashMap<>();
-          for(String key : keys)
+          for(String key : keys) {
                map.put(key, outConfig.get(key));
-
+               //System.out.println(key+" of class "+map.get(key).getClass()+" :: "+map.get(key).toString());
+          }
           inStream = MainCore.instance.getClass().getResourceAsStream("/config.yml");
           inStreamReader = new InputStreamReader(inStream);
 
           FileWriter fw = new FileWriter(outFile, false);
           BufferedReader br = new BufferedReader(inStreamReader);
           String line;
-          while((line = br.readLine()) != null) {
+          while((line = br.readLine()) != null)
                fw.write(line+'\n');
-               System.out.println(line);
-          }
           br.close();
           fw.close();
 
           outConfig = YamlConfiguration.loadConfiguration(outFile);
           for(String key : map.keySet())
-               if(outConfig.contains(key))
-                    if(map.get(key) instanceof String)
-                         outConfig.set(key, "\'"+map.get(key)+"\'");
-                    else
-                         outConfig.set(key, map.get(key));
+               if (outConfig.contains(key))
+                    outConfig.set(key, map.get(key));
           outConfig.save(outFile);
 
      }
